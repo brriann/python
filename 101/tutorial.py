96542698acfe3cceb99254575f6e5be7bd54df59
Brian Foster <brriann@gmail.com>
2024-03-24 20:26:46 -0600

    py tutorial

---
+++ 101/tutorial.py
@@ -0,0 +1,93 @@
+# https://docs.python.org/3/tutorial/
+# TUTORIAL NOTES
+
+# import sys
+# sys.argv[0]
+# 
+# >>> interactive mode (from a TTY/REPL)
+# 
+# https://docs.python.org/3/tutorial/introduction.html
+# 
+# / division vs // floor division (discards fractional part)
+# % modulo operator (remainder of division)
+# ** power x ** 2 x squared
+# + string concat
+# strings are immutable
+# 
+# id() built-in function that returns unique id of an object (integer representing memory address of object)
+# 
+# = assignment never copies data.
+# list1 = [1, 2, 3]
+# list2 = list1
+# id(list2) == id(list1) >>> True (variable list2 refers to existing list)
+# 
+# slice operations return a new list containing requested elements
+# list3 = list1[:] >>> list3 is a shallow copy
+# 
+# list1 = [1, 2, 3]
+# list2 = list1
+# print(id(list2) == id(list1))
+# list3 = list1[:]
+# print(list3)
+# print(id(list3) == id(list1))
+# 
+# assignment to slices is possible
+# 
+# letters = ['a', 'b', 'c', 'd']
+# letters[1:3] = ['B', 'C']
+# print(letters)
+# # len() function built-in (strings and lists)
+# print(len(letters))
+# # clear entire list
+# letters[:] = []
+# print(letters)
+# 
+# multi-assignment Fibonnaci example
+# a, b = 0, 1
+# while a < 50:
+#     print(a)
+#     a, b = b, a + b
+# 
+# multi-arg print() example
+i = 256 * 256
+print('the value of i is', i)
+# 
+# https://docs.python.org/3/tutorial/controlflow.html
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # python
 
 https://docs.python.org/3/tutorial/
+
+## standard library
+
+https://docs.python.org/3/library/index.html#library-index
+
+## language reference
+
+https://docs.python.org/3/reference/index.html#reference-index
+
+## extending / C & C++ / CPython
+
+https://docs.python.org/3/extending/index.html#extending-index
+
+## Python / C API reference
+
+https://docs.python.org/3/c-api/index.html#c-api-index
